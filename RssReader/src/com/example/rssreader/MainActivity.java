dc118d6f6107c1d5417829173d0743a8ac04649c
Dima Posashkov <k0kak0la1488@gmail.com>
2014-04-23 20:12:39 +0400

    Delete listviewitem.xml; Add ListView;

--- RssReader/src/com/example/rssreader/MainActivity.java
+++ RssReader/src/com/example/rssreader/MainActivity.java
@@ -4,24 +4,27 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-	private String finalUrl = "http://news.yandex.ru/hardware.rss";
+	private String finalUrl = "http://tutorialspoint.com/android/sampleXML.xml";
 	private HandleXml obj;
 	private EditText title, link, description, postDate;
+	private ListView lv;
 	private List<RssItem> rssItems;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		title = (EditText) findViewById(R.id.editTextTitle);
-		link = (EditText) findViewById(R.id.editTextLink);
-		description = (EditText) findViewById(R.id.editTextDescription);
-		postDate = (EditText) findViewById(R.id.editTextPubDate);
+		lv = (ListView) findViewById(R.id.listView1);
+//		title = (EditText) findViewById(R.id.editTextTitle);
+//		link = (EditText) findViewById(R.id.editTextLink);
+//		description = (EditText) findViewById(R.id.editTextDescription);
+//		postDate = (EditText) findViewById(R.id.editTextPubDate);
 	}
 
 	public void fetch(View view) {
@@ -30,15 +33,17 @@ public class MainActivity extends Activity {
 		while (!obj.isParsingComplite())
 			;
 		rssItems = obj.getItems();
-		
-		title.setText(rssItems.get(0).getTitle());
-		Log.e("handleXML", rssItems.get(0).getTitle());
-		
-		link.setText(rssItems.get(0).getLink());
-		Log.e("handleXML", rssItems.get(0).getLink());
-		
-		description.setText(rssItems.get(0).getDescription());
-		Log.e("handleXML", rssItems.get(0).getDescription());
+		ArrayAdapter<RssItem> adapter = new ArrayAdapter<RssItem>(this,
+				android.R.layout.simple_list_item_1, rssItems);
+		lv.setAdapter(adapter);
+		// title.setText(rssItems.get(0).getTitle());
+		// Log.e("handleXML", rssItems.get(0).getTitle());
+		//
+		// link.setText(rssItems.get(0).getLink());
+		// Log.e("handleXML", rssItems.get(0).getLink());
+		//
+		// description.setText(rssItems.get(0).getDescription());
+		// Log.e("handleXML", rssItems.get(0).getDescription());
 		// postDate.setText(obj.getPubDate());
 	}
 
