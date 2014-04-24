3f79a66a8cd93c498aef3fa9e5fb739f4442709d
Dima Posashkov <k0kak0la1488@gmail.com>
2014-04-24 09:57:42 +0400

    Add new activity screen

---
+++ RssReader/src/com/example/rssreader/DescriptionScreen.java
@@ -0,0 +1,15 @@
+package com.example.rssreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DescriptionScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.description_activity);
+	}
+
+}
